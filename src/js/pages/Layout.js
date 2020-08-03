5a406afce62ff1feca5efb8184a7a9418ec77ecb
billyshears <63904750+billyshearson@users.noreply.github.com>
2020-08-03 12:39:13 +0900

    URLSearchParams

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -1,7 +1,41 @@
 import React from "react";
+import { NavLink, Link, withRouter } from "react-router-dom";
 
-export default class Layout extends React.Component {
+class Layout extends React.Component {
+  navigate() {
+    console.log(this.props.history);
+    this.props.history.push("/");
+  }
   render() {
-    return <h1> Killer Whales </h1>;
+    return (
+      <div>
+        <h1> Killer Whales </h1>
+        {this.props.children}
+        <Link
+          to="/archives/some-other-articles?date=yesterday&filter=none"
+          class="btn btn-warning"
+        >
+          archives (some other articles)
+        </Link>
+        <Link to="/archives?date=today&filter=hot" class="btn btn-danger">
+          archives
+        </Link>
+        <NavLink
+          to="/settings/main"
+          class="btn btn-success"
+          activeClassName="btn btn-danger"
+        >
+          settings
+        </NavLink>
+        <Link to="/settings/extra" class="btn btn-success">
+          settings (extra)
+        </Link>
+        <button className="btn btn-info" onClick={this.navigate.bind(this)}>
+          featured
+        </button>
+      </div>
+    );
   }
 }
+
+export default withRouter(Layout);
